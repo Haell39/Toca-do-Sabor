f900817a4af7a3d27c545399ea98435ccafdf611
Rael <109318123+Haell39@users.noreply.github.com>
2024-07-02 09:19:19 -0300

    Atualizar o process_feedback.php

--- Paginas/process_feedback.php
+++ Paginas/process_feedback.php
@@ -1,25 +1,28 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Verifica se o método de solicitação é POST
     $name = htmlspecialchars($_POST['name']);
     $feedback = htmlspecialchars($_POST['feedback']);
 
-    // Defina o caminho para o arquivo feedback.txt
+    // Sanitiza os dados recebidos do formulário para evitar XSS
     $file = 'feedback.txt';
 
-    // Prepare a string a ser gravada no arquivo
+    // Define o caminho para o arquivo feedback.txt
     $data = "Nome: $name\nFeedback: $feedback\n------------------\n";
 
-    // Tente abrir o arquivo para adicionar dados
+    // Prepara a string a ser gravada no arquivo
     if (file_put_contents($file, $data, FILE_APPEND | LOCK_EX)) {
+        // Tenta abrir o arquivo para adicionar dados
         $message = 'Feedback enviado com sucesso!';
     } else {
         $message = 'Erro ao enviar o feedback. Tente novamente.';
     }
 
-    // Redirecione de volta à página principal com uma mensagem de feedback
+    // Redireciona de volta à página principal com uma mensagem de feedback
     header('Location: index.php?message=' . urlencode($message));
     exit;
 } else {
+    // Redireciona para a página principal se a solicitação não for POST
     header('Location: index.php');
     exit;
 }
